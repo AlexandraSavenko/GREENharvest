3c363dc0ded483159374939b59ea0af7953c6756
Sasha Savenko <sashasavenko3@gmail.com>
2025-03-24 20:24:15 +0200

    hero text media rules

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
 // import css from "./Hero.module.css"
 import '../scss/Hero.scss'
 export default function Hero () {
-    return <section>
-     <div className="container">
-     <h1 className="heroTitle">ORGANIC VEGETABLES TO YOUR DIET TODAY!</h1>
-     <p className="heroText">Our expertly curated vegetable baskets are made with the freshest, 
+    return <section className='hero-section'>
+        <div className="hero">
+     <div className="container hero-container">
+        <div className="hero-content">
+     <h1 className="hero-title">ORGANIC VEGETABLES TO YOUR DIET TODAY!</h1>
+     <p className="hero-text">Our expertly curated vegetable baskets are made with the freshest, 
         highest quality vegetables available. Fresh Harvest Box has got you covered. Fresh, high-quality 
         vegetables in expertly curated vegetable baskets delivered to you.</p>        
         </div>
-        <div className="heroImage"></div>
+        </div>
+        </div>
          </section>
 }
