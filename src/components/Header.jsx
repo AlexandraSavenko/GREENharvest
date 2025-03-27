7ff66c62c9840df9c5d1bc0fae3550ee327d9fb3
Sasha Savenko <sashasavenko3@gmail.com>
2025-03-27 11:06:23 +0200

    close button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,25 @@ import '../scss/Header.scss'
 import { useState } from "react";
 import clsx from "clsx";
 import Burger from '../images/burger-min.svg'
+import { IoClose } from "react-icons/io5";
+
 
 export default function Header (){
     const [navOpen, setNavOpen] = useState(false)
     return <header>
         <div className="container">
             <div className="header-wrap">
-        <Logo/>
+        <Logo className='header-logo'/>
         <div className="b-menu" onClick={()=>{
             setNavOpen(!navOpen)
         }}>
             <Burger/>
         </div>
         <nav className={clsx('header-nav', navOpen && 'open')}>
+        <IoClose className="close-menu-btn" onClick={()=>{
+            setNavOpen(!navOpen)
+        }} />
+
             <ul className="header-menu-list">
                 <li><a className="header-menu-link" href='#how'>how it works</a></li>
                 <li><a className="header-menu-link" href='#veges'>vegetables</a></li>
