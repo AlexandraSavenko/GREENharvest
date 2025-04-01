e1224f757b7378d2fca53f886f7cb28a94d80d9b
Sasha Savenko <sashasavenko3@gmail.com>
2025-04-01 10:13:46 +0300

    some styles added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../scss/Footer.scss'
 export default function Footer () {
     return <div className='footer'>
          <div className="container">
-            <div className='footerContent'></div>
+            <div className='footerContent'>
         <div className="footerLogo">
             <Logo/>
             <p className='footerText'>Whether you are looking for a healthy snack or a thoughtful gift, our fruit baskets are the perfect choice.</p>
@@ -11,6 +11,7 @@ export default function Footer () {
     <div className='footerContacts'>
         <p>+380 (68) 443-94-26</p>
         <p>1678 S.Pioneer Road Salt Lake City UT 84104</p>
+    </div>
     </div>
     <div className='footerPolicy'>
         <p>Privacy Policy / Cooking settings</p>
